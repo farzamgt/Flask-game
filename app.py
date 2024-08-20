19929f7a135e6bc3426476266d0d65ab79f3e896
Farzam Farin <farzamgt@gamil.com>
2024-08-20 17:48:01 +0300

    Styles added for all pages. app.py has been corrected.

--- app.py
+++ app.py
@@ -13,8 +13,15 @@ rounds = []
 
 @app.route('/', methods=['GET', 'POST'])
 def start():
-    global rounds
-    rounds = []  # Reset rounds for a new session
+    global rounds, user_score, computer_score, counter, final_user_score, final_computer_score
+
+    rounds = []
+    user_score = 0
+    computer_score = 0
+    counter = 0
+    final_user_score = 0
+    final_computer_score = 0
+
     if request.method == 'POST':
         return redirect(url_for('game'))
     return render_template('start.html')
@@ -23,7 +30,7 @@ def start():
 def game():
     global user_score, computer_score, counter, final_computer_score, final_user_score, rounds
 
-    result = None
+    result = 'No results yet!'
     user_choice = None
     computer_choice = None
     if counter < 3:
@@ -43,21 +50,21 @@ def game():
             computer_choice = options[random_number]
 
             if user_choice == computer_choice:
-                result = "No one wins this round"
+                result = "No one wins!"
             elif user_choice == "rock" and computer_choice == "scissors":
-                result = "You win this round!"
+                result = "You win!"
                 user_score += 1
                 counter += 1
             elif user_choice == "paper" and computer_choice == "rock":
-                result = "You win this round!"
+                result = "You win!"
                 user_score += 1
                 counter += 1
             elif user_choice == "scissors" and computer_choice == "paper":
-                result = "You win this round!"
+                result = "You win!"
                 user_score += 1
                 counter += 1
             else:
-                result = "Computer wins this round!"
+                result = "Computer wins!"
                 computer_score += 1
                 counter += 1
 
@@ -74,19 +81,15 @@ def game():
             final_computer_score += 1
             winner = "Computer won the game!"
 
-        if final_user_score >= 5:
+        if final_user_score >= 3:
             winner = "Congratulations! You are the overall winner!"
-            final_user_score = 0
-            final_computer_score = 0
-        elif final_computer_score >= 5:
+        elif final_computer_score >= 3:
             winner = "Computer is the overall winner!"
-            final_user_score = 0
-            final_computer_score = 0
         
         user_score = 0
         computer_score = 0
         counter = 0
-        rounds = []
+        rounds = []        
 
         return render_template('result.html', winner=winner, user_score=final_user_score, computer_score=final_computer_score)
 
